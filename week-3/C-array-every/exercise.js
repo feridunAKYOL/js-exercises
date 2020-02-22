619375a3eea66469e6c8776522cc611df867db8b
feridun Akyol <feridunakyol34@gmail.com>
2020-02-22 22:59:04 +0100

    week-3 is done

--- week-3/C-array-every/exercise.js
+++ week-3/C-array-every/exercise.js
@@ -2,15 +2,18 @@
    This program should check if the array `group` contains only students
 */
 
-var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
-var group = ["Austine", "Dany", "Swathi", "Daniel"];
+var students = [ 'Omar', 'Austine', 'Dany', 'Swathi', 'Lesley', 'Rukmini' ];
+var group = [ 'Austine', 'Dany', 'Swathi', 'Daniel' ];
+function IsStudent() {
+	return students.includes();
+}
 
-var groupIsOnlyStudents; // complete this statement
+var groupIsOnlyStudents = group.every(IsStudent); // complete this statement
 
 if (groupIsOnlyStudents) {
-  console.log("The group contains only students");
+	console.log('The group contains only students');
 } else {
-  console.log("The group does not contain only students");
+	console.log('The group does not contain only students');
 }
 
 /* EXPECTED RESULT */
--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -1,5 +1,20 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+var numbers = [ 0.1, 0.2, 0.3, 0.4, 0.5 ];
+console.log(numbers.map((x) => x * 100));
 
+// second solution
+
+function multiple100(x) {
+	return x * 100;
+}
+console.log(numbers.map(multiple100));
+
+// third solution
+
+console.log(
+	numbers.map(function mult(x) {
+		return x * 100;
+	})
+);
